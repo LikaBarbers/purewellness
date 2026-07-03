20abb7250107b06f4ada4bc5973cf192c55936c3
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 16:07:37 +0200

    Add footer section to footer.php

---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+<footer>
+
+<div class="container footer">
+
+<div>
+
+<h3>Purewellness.al</h3>
+
+<p>
+
+Natural Wellness & Skincare
+
+</p>
+
+</div>
+
+<div>
+
+<h4>Kontakt</h4>
+
+<p>
+
+Rruga Osman Myderizi<br>
+
+Komuna e Parisit<br>
+
+Tiranë, Albania
+
+</p>
+
+</div>
+
+<div>
+
+<h4>Copyright</h4>
+
+<p>
+
+© <?php echo date("Y"); ?>
+
+Purewellness.al
+
+</p>
+
+</div>
+
+</div>
+
+</footer>
+
+</body>
+
+</html>
