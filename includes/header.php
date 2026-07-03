da29234f547be73549dfd80fb2ed804a4450783c
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 16:25:19 +0200

    Refactor header to use Bootstrap navbar structure

--- includes/header.php
+++ includes/header.php
@@ -8,9 +8,13 @@
 
 <title>Purewellness.al</title>
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+rel="stylesheet">
 
 <link rel="stylesheet" href="assets/css/style.css">
 
@@ -18,38 +22,122 @@
 
 <body>
 
-<header>
+<nav class="navbar navbar-expand-lg bg-white shadow-sm fixed-top">
 
 <div class="container">
 
-<a href="index.php" class="logo">
+<a class="navbar-brand" href="index.php">
 
-<img src="assets/images/logo.png" alt="Purewellness">
+<img src="assets/images/logo.png"
+height="70"
+alt="Purewellness">
 
 </a>
 
-<nav>
+<button
+class="navbar-toggler"
+type="button"
+data-bs-toggle="collapse"
+data-bs-target="#menu">
 
-<ul>
+<span class="navbar-toggler-icon"></span>
 
-<li><a href="index.php">Home</a></li>
+</button>
 
-<li><a href="products.php">Shop</a></li>
+<div
+class="collapse navbar-collapse"
+id="menu">
 
-<li><a href="#">Skincare</a></li>
+<ul class="navbar-nav ms-auto align-items-lg-center">
 
-<li><a href="#">Wellness</a></li>
+<li class="nav-item">
 
-<li><a href="about.php">About</a></li>
+<a class="nav-link" href="index.php">
 
-<li><a href="contact.php">Contact</a></li>
+Home
 
-<li><a href="cart.php">🛒</a></li>
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="products.php">
+
+Shop
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="#">
+
+Skincare
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="#">
+
+Wellness
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="about.php">
+
+About
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="contact.php">
+
+Contact
+
+</a>
+
+</li>
+
+<li class="nav-item ms-lg-3">
+
+<a class="nav-link"
+href="#">
+
+<i class="bi bi-search"></i>
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link"
+href="cart.php">
+
+<i class="bi bi-bag"></i>
+
+</a>
+
+</li>
 
 </ul>
 
-</nav>
+</div>
 
 </div>
 
-</header>
+</nav>
+
+<div style="height:100px;"></div>
