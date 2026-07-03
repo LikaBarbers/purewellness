917877cb1e3161e2dc861bf381db7abb23370680
LikaBarbers <xhuljanolika1@gmail.com>
2026-07-03 15:20:45 +0200

    Add configuration file for Purewellness application

---
+++ includes/config.php
@@ -0,0 +1,31 @@
+<?php
+
+// ================================
+// Purewellness.al Configuration
+// ================================
+
+// Database
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'purewellness');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Website
+
+define('SITE_NAME', 'Purewellness.al');
+define('SITE_URL', 'http://localhost/purewellness');
+
+// Contact
+
+define('SITE_EMAIL', 'info@purewellness.al');
+define('SITE_PHONE', '+355');
+define('SITE_ADDRESS', 'Rruga Osman Myderizi, Komuna e Parisit, Tiranë, Albania');
+
+// Upload Folder
+
+define('UPLOAD_PATH', 'assets/uploads/');
+
+// Timezone
+
+date_default_timezone_set('Europe/Tirane');
